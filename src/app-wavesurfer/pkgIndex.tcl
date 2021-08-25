2d1cc7fbcbc874b76b89a7a0050f37bd14959c7f
Cécile Herr <31855012+cecileherr@users.noreply.github.com>
2021-08-25 21:47:58 +0200

    Added original src files Wavesurfer 1.8.8.4

---
+++ src/app-wavesurfer/pkgIndex.tcl
@@ -0,0 +1,4 @@
+# Automatically generated package index file
+# date   : Fri Dec 30 09:08:43 CET 2011
+# cmdline: build/mkPkgIndex.tcl src/app-wavesurfer/wavesurfer.tcl
+package ifneeded app-wavesurfer 1.8 [list source [file join $dir wavesurfer.tcl]]
